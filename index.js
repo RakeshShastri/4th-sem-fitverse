4d8840f5619ce98b98f7dc0ac91e7b2a43e431fc
DESKTOP-4HOPBN5\rrsro <rrsrocks19@gmail.com>
2022-07-25 00:01:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var express = require('express')
+var app = express()
+const port = 8080;
+
+var mysql = require('mysql');
+
+//database connection
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "test"
+});
+
+app.use(express.json());
+   
+// For serving static HTML files
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+   
+app.get("/", (req, res) => {
+  res.set({
+    "Allow-access-Allow-Origin": "*",
+  });
+     
+  // res.send("Hello World");
+  return res.redirect("index.html");
+});
+
+//Post Request Handler for recieving data
+app.post("/SignUpForm", (req, res) => {
+    console.log(req.body.name);
+    console.log(req.body.email);
+    console.log(req.body.phone);
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = `INSERT INTO test VALUES ('${req.body.name}', '${req.body.email}', '${req.body.phone}' )`;
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("1 record inserted");
+        });
+      });
+    
+})
+
+app.listen(port, () => {
+    console.log(`The application started 
+    successfully on port ${port}`);
+  });
